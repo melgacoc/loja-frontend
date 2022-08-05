b65f9729eb4d25f10f735fbad30cc3f4e4ada1df
Samuel Hyrum <samuelhymp@gmail.com>
2022-08-05 20:00:09 -0300

    Requisito 5 com erros

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
     homeInput: '',
     renderAlert: false,
+    filteredProducts: [],
+    produtcsAlert: false,
   }
 
   validate = ({ target }) => {
@@ -20,22 +23,73 @@ class Home extends React.Component {
     }
   }
 
+  searchProduct = async () => {
+    const { homeInput } = this.state;
+    const response = await getProductsFromCategoryAndQuery(homeInput, homeInput);
+    console.log('test');
+    console.log(response);
+    if (response.results.length === 0) {
+      console.log('test');
+      return (
+        this.setState({
+          produtcsAlert: false,
+          renderAlert: false,
+          homeInput: '',
+        })
+      );
+    }
+    this.setState({
+      filteredProducts: response,
+      produtcsAlert: true,
+      homeInput: '',
+      renderAlert: false,
+    });
+  }
+
   render() {
-    const { homeInput, renderAlert } = this.state;
+    const { homeInput, renderAlert, filteredProducts, produtcsAlert } = this.state;
     const text = 'Digite algum termo de pesquisa ou escolha uma categoria.';
+    const { results } = filteredProducts;
+
     return (
       <div>
         <label htmlFor="home-search-input">
           <input
             id="home-search-input"
             type="text"
+            data-testid="query-input"
             name="homeInput"
             value={ homeInput }
             onChange={ this.validate }
           />
+          <button
+            data-testid="query-button"
+            name="Pesquisar"
+            value="Pesquisar"
+            type="button"
+            // disabled={ buttonDisable }
+            onClick={ this.searchProduct }
+          >
+            Pesquisar
+          </button>
         </label>
         { !renderAlert
         && <p data-testid="home-initial-message">{ text }</p>}
+
+        {produtcsAlert ? (results.map((products) => (
+          <div
+            key={ products.id }
+          >
+            <p data-testid="product">{products.title}</p>
+            <img
+              data-testid="product"
+              src={ products.thumbnail }
+              alt={ products.title }
+            />
+            <p data-testid="product">{products.price}</p>
+          </div>
+        ))) : (<p> Nenhum produto foi encontrado </p>) }
+
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,9 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(url);
+  console.log(url);
   const object = await response.json();
   return object;
 }
